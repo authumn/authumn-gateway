7d9f23cc865e0c931e8c0f858e82d57820460b93
Rob Halff <rob.halff@gmail.com>
2018-05-31 20:51:33 +0200

    spit out generated nginx.conf

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ envsubst \
    conf/nginx.conf.template > \
    conf/nginx.conf
 
+cat conf/nginx.conf
+
 /usr/local/openresty/bin/openresty -g 'daemon off; error_log /dev/stderr info;' -c /conf/nginx.conf
