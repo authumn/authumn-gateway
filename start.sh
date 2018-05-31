a049bb6524b057c4d64689a6f8d676a88877e598
Rob Halff <rob.halff@gmail.com>
2018-05-31 20:42:07 +0200

    add resolver to envsubst

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 envsubst \
-  '$TOKEN_URL $USER_URL $API_URL $SERVER_NAME $MOUNT_POINT' < \
+  '$TOKEN_URL $USER_URL $API_URL $SERVER_NAME $MOUNT_POINT $RESOLVER' < \
    conf/nginx.conf.template > \
    conf/nginx.conf
 
